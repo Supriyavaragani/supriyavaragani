8a5bfde29a0c8ebe1b829664437a519802b09e56
Supriyavaragani <136726388+Supriyavaragani@users.noreply.github.com>
2024-06-06 10:42:31 +0530

    Update jdbd delete.java

--- 31-05-2023/jdbc delete/jdbd delete.java
+++ 31-05-2023/jdbc delete/jdbd delete.java
@@ -9,9 +9,9 @@ public class JdbcDelete {
 public static void main(String args[]) throws SQLException {
 	    try {
 	    Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techm?useSSL=false", "root", "Sravan@18");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techm?useSSL=false", "root", "Supriya@18");
 		Statement stmt= con.createStatement();
-			String str="Supriya";
+			String str="Rohini";
 			int s = stmt.executeUpdate("delete from customers where customername='" + str + "'");
 			System.out.println("Number of rows affected is" + s);
 	    }
